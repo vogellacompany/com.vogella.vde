41053941a829ce945c973c99fbbfbbbc3dbb2b16
Lars Vogel <Lars.Vogel@gmail.com>
2013-12-18 16:41:53 +0100

    New voclipse perspective

---
+++ com.vogella.vde.perspective.java/src/com/vogella/vde/perspective/java/PerspectiveFactory1.java
@@ -0,0 +1,13 @@
+package com.vogella.vde.perspective.java;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class PerspectiveFactory1 implements IPerspectiveFactory {
+
+	@Override
+	public void createInitialLayout(IPageLayout layout) {
+
+	}
+
+}
